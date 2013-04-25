9bbaf92214159a35cab3b477c22bd26f87adb0da
Manuel García <mgarciap@gmail.com>
2013-04-25 10:54:29 -0300

    Added openjdk-6-jdk to UAA packages

--- chef/uaa/recipes/packages.rb
+++ chef/uaa/recipes/packages.rb
@@ -1,11 +1,8 @@
 packages = %w(
+  openjdk-6-jdk
   maven
 
 )
-
-# TODO: Add sun-java6-jdk
-# The problem is that we need to answer ok to
-# license agreement
 
 packages.each do |package_name|
   package package_name do
