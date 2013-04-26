e3b1dec0156c6340d847f2085acc1fd90ab7b530
Joaquín Vicente <joakin@gmail.com>
2013-04-26 16:10:36 -0300

    Add recipe steps to clone CF modules

--- chef/cc_ng/recipes/install.rb
+++ chef/cc_ng/recipes/install.rb
@@ -1,3 +1,9 @@
+execute "git clone cloud_controller_ng" do
+  cwd "/cf-deploy"
+  command "git clone https://github.com/cloudfoundry/cloud_controller_ng.git"
+  action :run
+end
+
 execute "install cloud_controller_ng gems" do
   cwd "/cf-deploy/cloud_controller_ng"
   command "bundle install"
--- chef/uaa/recipes/install.rb
+++ chef/uaa/recipes/install.rb
@@ -1,3 +1,9 @@
+execute "git clone uaa" do
+  cwd "/cf-deploy"
+  command "git clone https://github.com/cloudfoundry/uaa.git"
+  action :run
+end
+
 execute "install java uaa packages" do
   cwd "/cf-deploy/uaa"
   command "mvn clean install"
