1aba11bed2395626005c856bc8bb65212112d3d8
Manuel García <mgarciap@gmail.com>
2013-04-26 12:28:14 -0300

    refined and improve documentation on commands

--- chef/uaa/recipes/install.rb
+++ chef/uaa/recipes/install.rb
@@ -1,5 +1,11 @@
-execute "install java uaa packages" do
+execute "uaa - maven install" do
   cwd "/cf-deploy/uaa"
-  command "mvn clean install"
+  command "mvn clean install -Dmaven.test.skip=true"
+  action :run
+end
+
+execute "uaa - generating packages" do
+  cwd "/cf-deploy/uaa"
+  command "mvn package -Dmaven.test.skip=true"
   action :run
 end
