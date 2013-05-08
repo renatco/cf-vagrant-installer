8fdeaad7a6fa539872437e662f25a87c69ac80cc
Manuel García <mgarciap@gmail.com>
2013-05-08 10:20:47 -0300

    work in progress

--- chef/cc_ng/recipes/install.rb
+++ chef/cc_ng/recipes/install.rb
@@ -1,17 +1,25 @@
-git "/cf-deploy/cloud_controller_ng" do
-  repository "https://github.com/cloudfoundry/cloud_controller_ng.git"
-  revision "db2de6f2f0c26858cccfa55a544dc3a2500f1173"
-  action :sync
+PATH = "/cf/cloud_controller_ng"
+
+execute "install and update git submodules" do
+  user 'travis'
+  cwd PATH
+  command "git submodule update --init --recursive && touch /tmp/cc_submodules_installed.ack"
+  action :run
+  not_if do ::File.exists?('/var/cc_submodules_installed.ack') end
 end
 
 execute "install cloud_controller_ng gems" do
-  cwd "/cf-deploy/cloud_controller_ng"
-  command "bundle install"
+  cwd PATH
+  user 'travis'
+  command "bundle > bundle.log && touch /tmp/cc_bundle_install.ack"
   action :run
+  not_if do ::File.exists?('/var/cc_bundle_install.ack') end
 end
 
 execute "run db:migrate" do
-  cwd "/cf-deploy/cloud_controller_ng"
-  command "bundle exec rake db:migrate"
+  cwd PATH
+  user 'travis'
+  command "bundle exec rake db:migrate && touch /tmp/cc_db_migrate.ack"
   action :run
+  not_if do ::File.exists?('/var/cc_db_migrate.ack') end
 end
