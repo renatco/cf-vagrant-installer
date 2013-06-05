e2fff96a13a3541849a5ad87f025fbfecacd2272
Scott Frederick <scottyfred@gmail.com>
2013-06-05 14:12:52 -0500

    Cleanup warden initialization. Small enhancements to sample Sinatra app.

--- chef/cloudfoundry/recipes/warden.rb
+++ chef/cloudfoundry/recipes/warden.rb
@@ -1,8 +1,5 @@
-WARDEN_PATH = "/vagrant/warden/warden"
 ROOT_FS = "/var/warden/rootfs"
 ROOT_FS_URL = "http://cfstacks.s3.amazonaws.com/lucid64.dev.tgz"
-OLD_CONFIG_FILE_PATH = "#{WARDEN_PATH}/config/linux.yml"
-NEW_CONFIG_FILE_PATH = "#{WARDEN_PATH}/config/test_vm.yml"
 
 package "quota" do
   action :install
@@ -18,16 +15,6 @@ end
 
 execute "remove remove all remnants of apparmor" do
   command "sudo dpkg --purge apparmor"
-end
-
-ruby_block "configure warden to put its rootfs outside of /tmp" do
-  block do
-    require "yaml"
-    config = YAML.load_file(OLD_CONFIG_FILE_PATH)
-    config["server"]["container_rootfs_path"] = ROOT_FS
-    File.open(NEW_CONFIG_FILE_PATH, 'w') { |f| YAML.dump(config, f) }
-  end
-  action :create
 end
 
 execute "download warden rootfs from s3" do
