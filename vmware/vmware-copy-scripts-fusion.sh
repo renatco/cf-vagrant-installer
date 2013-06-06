16911074d5be916a943e94f65a3a041a36a6b063
Joe Fitzgerald <joe.fitzgerald@emc.com>
2013-06-06 11:49:42 -0600

    Updating to prevent file disappearance in second step

--- vmware/vmware-copy-scripts-fusion.sh
+++ vmware/vmware-copy-scripts-fusion.sh
@@ -5,5 +5,5 @@ set -o nounset
 
 precise_vmx=$(/Applications/VMware\ Fusion.app/Contents/Library/vmrun list | fgrep precise64.vmx)
 
-/Applications/VMware\ Fusion.app/Contents/Library/vmrun -gu vagrant -gp vagrant copyFileFromHostToGuest "$precise_vmx" vmware-setup1.sh /tmp/vmware-setup1.sh
-/Applications/VMware\ Fusion.app/Contents/Library/vmrun -gu vagrant -gp vagrant copyFileFromHostToGuest "$precise_vmx" vmware-setup2.sh /tmp/vmware-setup2.sh
+/Applications/VMware\ Fusion.app/Contents/Library/vmrun -gu vagrant -gp vagrant copyFileFromHostToGuest "$precise_vmx" vmware-setup1.sh /home/vagrant/vmware-setup1.sh
+/Applications/VMware\ Fusion.app/Contents/Library/vmrun -gu vagrant -gp vagrant copyFileFromHostToGuest "$precise_vmx" vmware-setup2.sh /home/vagrant/vmware-setup2.sh
