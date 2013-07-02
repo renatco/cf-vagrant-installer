455ea410133c4880199833aff5b20e7e1f60d538
Manuel Garcia <mgarciap@gmail.com>
2013-07-02 20:50:32 -0300

    Detect routes registering listening to NATs and write them down for avahi perl process to publish it

---
+++ avahi/router_register_listener.rb
@@ -0,0 +1,47 @@
+#!/usr/bin/env ruby
+
+require "nats/client"
+require "json"
+
+ALIAS_FILE = '/vagrant/avahi/aliases'
+
+def wait_for_nats_to_start()
+  Timeout::timeout(10) do
+    loop do
+      sleep 0.2
+      break if nats_up?
+    end
+  end
+end
+
+def nats_up?
+  NATS.start do
+    NATS.stop
+    return true
+  end
+rescue NATS::ConnectError
+  nil
+end
+
+def initialize
+  File.open(ALIAS_FILE, 'w') {}
+  wait_for_nats_to_start()
+end
+
+def update_aliases(uri)
+  File.open(ALIAS_FILE, 'r+') do |file|
+    file.each.any? do |line|
+      uri.eql? line.chomp
+    end or file.puts uri
+  end
+end
+
+
+#TODO still have to manage the router.unregister messages
+NATS.start do
+  sid = NATS.subscribe('router.register') do |response|
+    route = JSON.parse(response)
+    uri = route["uris"].first
+    update_aliases(uri)
+  end
+end
