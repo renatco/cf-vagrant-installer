d2c46a889d80de1033eaf8cc525a46ccbc48c815
Manuel Garcia <mgarciap@gmail.com>
2013-07-26 22:03:55 -0300

    generate xip.io domain by getting the instance public IP. Configure CF to use that domain

--- chef/cloudfoundry/recipes/vagrant-provision-end.rb
+++ chef/cloudfoundry/recipes/vagrant-provision-end.rb
@@ -6,3 +6,16 @@ bash "emit provision complete" do
         /sbin/initctl emit --no-wait vagrant-provisioner-complete
     END_OF_SCRIPT
 end
+
+
+bash "AWS DNS setup" do
+  user "root"
+  cwd "/vagrant/custom_config_files"
+  code <<-END_OF_SCRIPT
+        EXT_IP=$(curl http://instance-data/latest/meta-data/public-ipv4)
+        XIP_IO_HOST=$EXT_IP.xip.io
+        XIP_IO_SHORT=$(host $XIP_IO_HOST| head -1 | cut  -d ' '  -f6 | rev |cut -c 2- | rev)
+        find . -type f -print0 | xargs -0 sed -i 's/vcap.me/'"$XIP_IO_SHORT"'/g'
+  END_OF_SCRIPT
+  only_if { node[:virtualization][:system] == "xen" }
+end
