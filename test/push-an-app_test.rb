f0a732ebf104e0c52fcac760db791a145d304a71
Gaston Ramos <ramos.gaston@gmail.com>
2013-08-09 16:27:33 -0300

    added test that get the ruby app via http

--- test/push-an-app_test.rb
+++ test/push-an-app_test.rb
@@ -1,4 +1,5 @@
 require "minitest/autorun"
+require 'net/http'
 
 class CustomMiniTest
   class Unit < MiniTest::Unit
@@ -10,7 +11,13 @@ end
 
 class TestPushAnApp < CustomMiniTest::Unit::TestCase
   def before_all
+    delete_all_apps!
     cf_login_and_set_space
+  end
+
+  def delete_all_apps!
+    return if system "cf apps" =~ /No applications/
+    system "yes | cf delete hello hello-node"
   end
 
   def cf_login_and_set_space
@@ -23,10 +30,15 @@ class TestPushAnApp < CustomMiniTest::Unit::TestCase
   end
 
   def test_we_can_push_a_ruby_app
-    assert push_test_app(:sinatra)
+    assert push_test_app :sinatra
+  end
+
+  def test_app_ruby_app_is_up
+    content = Net::HTTP.get URI('http://hello.vcap.me')
+    assert_match content, /Hello/
   end
 
   def test_we_can_push_a_nodejs_app
-    assert push_test_app(:nodejs)
+    assert push_test_app :nodejs
   end
 end
